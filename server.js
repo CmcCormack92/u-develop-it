03403f9641b3ce732fa2ec6845d4653077c22123
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2022-01-12 17:29:26 -0500

    queried database

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
+const mysql = require('mysql2');
+
 const app = express();
 
 //express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        // Your MySql password
+        password: 'cdm83092',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //Default response for any other request (Not Found)
 app.use((req, res) => {
